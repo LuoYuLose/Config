63e89aa3781bc02e756e3d1c04826273af130322
LuoYuLose <2843966358@qq.com>
2023-06-11 03:00:46 +0800

    回来了，更新Qtile配置（存在QtileBar圆角无法消除的情况，日后修复

--- qtile/config.py
+++ qtile/config.py
@@ -24,7 +24,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-from libqtile import bar, layout, widget, hook, qtile
+from libqtile import bar, layout, widget, hook, qtile, extension
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
 from libqtile.utils import guess_terminal
@@ -80,9 +80,23 @@ keys = [
     # Toggle between different layouts as defined below
     Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
     Key([mod], "q", lazy.window.kill(), desc="Kill focused window"),
-    Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
+    # 禁用热重载，避免bar出现问题
+    # Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-    Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
+    # Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
+    # 程序启动配置
+    Key([mod], "F1", lazy.spawn("firefox"), desc="火狐浏览器"),
+    Key([mod], "r", lazy.spawn("rofi -show drun"), desc="Rofi程序启动器"),
+    Key([mod], "m", lazy.spawn("/opt/YesPlayMusic/yesplaymusic"), desc="网易云三方音乐播放器"),
+    Key([mod, "Shift"], "m", lazy.spawn("qqmusic"), desc="QQ音乐"),
+
+    # 音频控制
+    Key([ ], "XF86AudioPlay", lazy.spawn("playerctl play-pause")),
+    Key([ ], "XF86AudioStop", lazy.spawn("playerctl stop")),
+    Key([ ], "XF86AudioPrev", lazy.spawn("playerctl previous")),
+    Key([ ], "XF86AudioNext", lazy.spawn("playerctl next")),
+    Key([ ], "XF86AudioRaiseVolume", lazy.spawn("pactl set-sink-volume 0 +10%")),
+    Key([ ], "XF86AudioLowerVolume", lazy.spawn("pactl set-sink-volume 0 -5%")),
 ]
 
 groups = [Group(i) for i in "123456789"]
@@ -128,7 +142,223 @@ layouts = [
 ]
 
 # Bar
-# 采用Polybar
+# # DEFAULT WIDGET SETTINGS
+# BLACK = '#29414f'
+# RED = '#ec5f67'
+# GREEN = '#99c794'
+# YELLOW = '#fac863'
+# BLUE = '#6699cc'
+# MAGENTA = '#c594c5'
+# CYAN = '#5fb3b3'
+# WHITE = '#ffffff'
+# 
+# widget_defaults = dict(
+#     font="Source Code Pro",
+#     fontsize=18,
+#     padding=2,
+#     background=BLACK
+# )
+# extension_defaults = widget_defaults.copy()
+# 
+# # WIDGETS
+# def init_xmenu():
+#     return widget.TextBox(
+#         text="",
+#         foreground=WHITE,
+#         background=BLACK,
+#         fontsize=32,
+#         padding=8,
+#         mouse_callbacks={
+#             'Button1': lambda: qtile.cmd_spawn([
+#                 '/bin/bash', os.path.expanduser(os.path.join(
+#                     '~', 'scripts', 'xmenu.sh'))
+#             ])
+#         }
+#     )
+# 
+# 
+# def init_group_box():
+#     return widget.GroupBox(
+#         fontsize=26,
+#         spacing=5,
+#         disable_drag=True,
+#         active=WHITE,
+#         inactive=WHITE,
+#         rounded=False,
+#         highlight_method="block",
+#         this_current_screen_border=GREEN,
+#         this_screen_border=CYAN,
+#         other_current_screen_border=BLACK,
+#         other_screen_border=BLACK,
+#         foreground=WHITE,
+#         background=BLACK,
+#         margin_x=6,
+#         margin_y=3,
+#         padding_x=4,
+#         padding_y=7,
+#     )
+# 
+# 
+# def init_check_updates():
+#     return widget.CheckUpdates(
+#         execute=' '.join(["kitty", '-e', 'paru']),
+#         distro='Arch_yay',
+#         display_format=' Updates: {updates}',
+#         update_interval=1800,
+#         foreground=WHITE,
+#         background=CYAN,
+#         padding=5
+#     )
+# 
+# 
+# def init_bitcoin_ticker():
+#     return widget.BitcoinTicker(
+#         # currency='USD',
+#         fmt=" {}",
+#         foreground=WHITE,
+#         background=GREEN,
+#         padding=5
+#     )
+# 
+# 
+# def init_thermal_sensor():
+#     return widget.ThermalSensor(
+#         fmt=' {}',
+#         tag_sensor="Package id 0",
+#         foreground=WHITE,
+#         background=CYAN,
+#         padding=5
+#     )
+# 
+# 
+# def init_volume():
+#     return widget.Volume(
+#         fmt=" {}",
+#         foreground=WHITE,
+#         background=GREEN,
+#         padding=5
+#     )
+# 
+# 
+# def init_current_layout_icon():
+#     return widget.CurrentLayoutIcon(
+#         custom_icon_paths=[os.path.expanduser(
+#             "$HOME/.config/qtile/icons")],
+#         foreground=WHITE,
+#         background=CYAN,
+#         scale=0.6
+#     )
+# 
+# 
+# def init_curren_layout():
+#     return widget.CurrentLayout(
+#         foreground=WHITE,
+#         background=CYAN,
+#         padding=5
+#     )
+# 
+# 
+# def init_clock():
+#     return widget.Clock(
+#         fmt=" {}",
+#         foreground=WHITE,
+#         background=GREEN,
+#         padding=5,
+#         format="%A, %d %B [ %H:%M ]"
+#     )
+# 
+# 
+# def init_wide_bar(tray=True):
+#     return [
+#         init_xmenu(),
+#         init_group_box(),
+#         widget.TaskList(),
+#         widget.Systray(
+#             background=BLACK,
+#             padding=5
+#         ) if tray else widget.Sep(linewidth=0),
+#         widget.Sep(
+#             linewidth=0,
+#             padding=5,
+#             background=BLACK
+#         ),
+#         init_check_updates(),
+#         init_bitcoin_ticker(),
+#         init_thermal_sensor(),
+#         init_volume(),
+#         init_current_layout_icon(),
+#         init_curren_layout(),
+#         init_clock(),
+#         widget.Sep(
+#             linewidth=0,
+#             padding=5,
+#             background=GREEN
+#         )
+#     ]
+# 
+# 
+# def init_short_bar():
+#     return [
+#         init_group_box(),
+#         widget.TaskList(),
+#         init_current_layout_icon(),
+#         init_curren_layout(),
+#         init_clock(),
+#         widget.Sep(
+#             linewidth=0,
+#             padding=5,
+#             background=GREEN
+#         )
+#     ]
+# 
+# 
+# bar_list = (
+#     init_wide_bar(),
+#     init_wide_bar(tray=False),
+#     init_short_bar(),
+# )
+# 
+# if __name__ in ["config", "__main__"]:
+#     screens = [
+#         Screen(top=bar.Bar(
+#             widgets=widgets, opacity=1, size=30, margin=[8, 8, 0, 8],
+#         )) for widgets in bar_list
+#     ]
+screens = [
+    Screen(
+        top=bar.Bar(
+            [
+                widget.CurrentLayout(),
+                widget.GroupBox(),
+                widget.Prompt(),
+                widget.WindowName(),
+                widget.Chord(
+                    chords_colors={
+                        "launch": ("#ff0000", "#ffffff"),
+                    },
+                    name_transform=lambda name: name.upper(),
+                ),
+                widget.TextBox("default config", name="default"),
+                widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
+                # NB Systray is incompatible with Wayland, consider using StatusNotifier instead
+                # widget.StatusNotifier(),
+                widget.Systray(),
+                widget.Clock(format="%Y-%m-%d %a %I:%M %p"),
+                widget.QuickExit(),
+            ],
+            24,
+            # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
+            # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
+        ),
+    ),
+]
+ 
+widget_defaults = dict(
+    font="Source Code Pro",
+    fontsize=18,
+    padding=3,
+)
+extension_defaults = widget_defaults.copy()
 
 # Drag floating layouts.
 mouse = [
---
+++ qtile/scripts/autostart-wayland.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# Wayland
+
+# mpvpaper
+mpvpaper -o "no-audio loop-file shuffle panscan=1.0 no-keepaspect no-input-default-bindings hwdec" '*' /home/luoyu/WallPapers/动态壁纸/
+
+# Fcitx5
+fcitx5 -d
+
+# Waybar
+waybar
